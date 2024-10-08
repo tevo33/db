218f1d29ae76292ec0f605657220d086b83bd4f1
tevo33 <tevoguerra@gmail.com>
2024-10-07 22:29:37 -0300

    Estruturação inicial

---
+++ src/test/java/com/project/db/DbApplicationTests.java
@@ -0,0 +1,14 @@
+package com.project.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class DbApplicationTests {
+
+
+    @Test
+    @Transactional
+	void test(){}
+}
